dbded8dbec12f5d695e07cac47810ea8c5739c98
Fahad18-web <fa2367701@gmail.com>
2026-05-22 21:05:22 +0500

    requirements.txt file added

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,11 +1,8 @@
 from collections import Counter , defaultdict
-from importlib.resources import path
-from urllib import response
-
-from streamlit import status
 import sys
+# *************** Problem 1: Basic Log File Reader ****************
+# Ye script log file ko read karta hai aur uska content console par print karta hai.
 
-# This script reads a log file and prints its contents to the console.
 # def read_log_file(file_path):
 #     with open(file_path, 'r') as f:
 #         for line in f:
@@ -13,7 +10,9 @@ import sys
 
 # read_log_file('server.log')            
 
-# This script reads a log file and coverts each line into list parts.
+# *************** Problem 2: Log File Parsing ****************
+# ab ye script log file ko read karta hai aur uska content formatted manner mein print karta hai.
+
 # def read_log_file(file_path):
 #     with open(file_path,'r') as f:
 #         for line in f:
@@ -22,8 +21,9 @@ import sys
 #             print(parts)
 # read_log_file('server.log')        
 
+# *************** Problem 3: Log File Analysis ****************
+# Uske bad ye script log file read kar ke lines parts ko variable mei store karta hai.
 
-# This script reads a log file and extracts specific parts of each line to print formatted output.
 # def read_log_file(file_path):
 #     with open(file_path,'r') as f:
 #         for line in f:
@@ -39,7 +39,10 @@ import sys
 
 # read_log_file('server.log')            
 
-# This script reads a log file and handles blank lines and lines with insufficient parts.
+
+# *************** Problem 4: Handling Blank Lines ****************
+# phr ye script log file ko read karta hai aur blank lines ko skip karta hai.
+
 # def read_log_file(file_path):
 #     with open(file_path,'r') as f:
 #         for line in f:
@@ -62,7 +65,10 @@ import sys
 #             print(f'IP Address: {ip_address} | Request Method: {request_method} | Status Code: {status_code}')
 # read_log_file('server.log')            
 
-# This script reads a log file, extracts specific parts of each line, and counts the occurrences of each status code.
+
+# *************** Problem 5: Status Code Counting ****************
+# ye second last script hai jo status codes ka count deta hai aur phr batata hai konsa status code sabse jyada aaya.
+
 # def read_log_file(file_path):
 #     status_counter = Counter()
 #     with open(file_path,'r') as f:
@@ -89,48 +95,51 @@ import sys
 #         print(f"Status {code}: {count} time comes")
 # read_log_file('server.log')        
 
-# This script reads a log file, extracts specific parts of each line, counts the occurrences of each status code, and identifies the top 3 slowest endpoints based on average response time.
-def read_log_file(file_path):
-    status_counter = Counter()
-    path_times = defaultdict(list)
-    with open(file_path,'r') as f:
-         for line in f:
-            line = line.strip()
-            if not line:
-                continue
-            parts = line.split(' ')
-            if len(parts) < 6:
-                print(f'Incomplete lines skipped: {line}')
-                continue
-            timestamp = parts[0]
-            ip_address = parts[1]
-            request_method = parts[2]
-            request_path = parts[3]
-            status_code = parts[4]
-            request_response = parts[5]    
-            # count status codes within loop
-            status_counter[status_code] += 1
 
-            if request_response.endswith('ms'):
-                ms = float(request_response.replace('ms', ''))
-                path_times[request_path].append(ms)
+# *************** Problem 6: Slowest Endpoints ****************
+# ye last script hai jo status codes ka count deta hai, aur phr batata hai konsa status code sabse jyada aaya, aur top 3 slowest endpoints identify karta hai.
 
-            print(f'IP Address: {ip_address} | Request Method: {request_method} | Status Code: {status_code}')
-    print("\n*** Status Code Report ***")
-    for code, count in status_counter.items():
-        print(f"Status {code}: {count} time comes")
-    print("\n*** Top 3 Slowest Endpoints ***")
-    # extract average times for each path
-    path_avg = {}
-    for p, times in path_times.items():
-        path_avg[p] = sum(times) / len(times)
+# def read_log_file(file_path):
+#     status_counter = Counter()
+#     path_times = defaultdict(list)
+#     with open(file_path,'r') as f:
+#          for line in f:
+#             line = line.strip()
+#             if not line:
+#                 continue
+#             parts = line.split(' ')
+#             if len(parts) < 6:
+#                 print(f'Incomplete lines skipped: {line}')
+#                 continue
+#             timestamp = parts[0]
+#             ip_address = parts[1]
+#             request_method = parts[2]
+#             request_path = parts[3]
+#             status_code = parts[4]
+#             request_response = parts[5]    
+#             # count status codes within loop
+#             status_counter[status_code] += 1
 
-    # sort paths by average response time in descending order
-    sorted_paths = sorted(path_avg.items(), key=lambda x: x[1], reverse=True)
+#             if request_response.endswith('ms'):
+#                 ms = float(request_response.replace('ms', ''))
+#                 path_times[request_path].append(ms)
 
-    for p, avg in sorted_paths[:3]:
-        print(f"{p} — Average: {avg:.1f}ms")    
-if len(sys.argv) < 2:
-       print("Usage: python log_analyzer.py <logfile>")
-else:
-       read_log_file(sys.argv[1])         
+#             print(f'IP Address: {ip_address} | Request Method: {request_method} | Status Code: {status_code}')
+#     print("\n*** Status Code Report ***")
+#     for code, count in status_counter.items():
+#         print(f"Status {code}: {count} time comes")
+#     print("\n*** Top 3 Slowest Endpoints ***")
+#     # extract average times for each path
+#     path_avg = {}
+#     for p, times in path_times.items():
+#         path_avg[p] = sum(times) / len(times)
+
+#     # sort paths by average response time in descending order
+#     sorted_paths = sorted(path_avg.items(), key=lambda x: x[1], reverse=True)
+
+#     for p, avg in sorted_paths[:3]:
+#         print(f"{p} — Average: {avg:.1f}ms")    
+# if len(sys.argv) < 2:
+#        print("Usage: python log_analyzer.py <logfile>")
+# else:
+#        read_log_file(sys.argv[1])         
